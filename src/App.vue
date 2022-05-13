4b93b12f6325589350a9ed9a695b8ebee878d92b
rosell-dk <bjorn@rosell.dk>
2022-05-13 15:48:40 +0200

    Center win text

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,6 @@ import TheGame from './components/TheGame.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
